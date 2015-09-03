57828400d581c9e7f265a14241b772f8fa7565e3
hd132611 <shinjs0371@naver.com>
2015-09-03 11:13:23 +0900

    33333333333333333333

---
+++ JavaPractice/src/org/dimigo/abstractclass/Galaxy.java
@@ -0,0 +1,28 @@
+package org.dimigo.abstractclass;
+
+/*
+ * <pre>
+ * org.dimigo.inheritance
+ * 		|_ Galaxy
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 28.
+ * </pre>
+ * @author 신준섭
+ *
+ */
+public class Galaxy extends SmartPhone{
+	public Galaxy(){
+		super();
+	}
+	public Galaxy(String model, String company, int price){
+		super(model, company, price);
+	}
+
+	public void pay(){
+		System.out.println("삼성 페이로 결제합니다.");
+	}
+	
+	public void useWirelessCharge(){
+		System.out.println("무선 충전 기능을 사용합니다.");
+	}	
+}
